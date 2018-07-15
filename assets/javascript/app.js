12b53984d4e8058dc4fd9304e6b3f7980a989033
L-Ward <noreply>
2018-07-15 14:16:59 -0700

    setting up oauth for meetup and basic html cdn setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,3 @@
+//OAuth2 seetup for meetup
+var meetupURL = "https://secure.meetup.com/oauth2/authorize?client_id=idg65cddfd4a7uhbd2c90841o7&response_type=token&redirect_uri=https://l-ward.github.io/Project1/"
+$(location).attr("href", meetupURL);
