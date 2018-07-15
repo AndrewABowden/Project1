73b93c2545a86d40098bc64f124968b35144adf5
L-Ward <noreply>
2018-07-15 14:39:54 -0700

    ajax request for meetup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,16 @@
-//OAuth2 seetup for meetup
-var meetupURL = "https://secure.meetup.com/oauth2/authorize?client_id=idg65cddfd4a7uhbd2c90841o7&response_type=token&redirect_uri=https://l-ward.github.io/Project1/"
-$(location).attr("href", meetupURL);
+//Checking for acces token
+if (location.href.indexOf("#") == -1) {
+    //OAuth2 seetup for meetup
+    var meetupAuthorizationURL = "https://secure.meetup.com/oauth2/authorize?client_id=idg65cddfd4a7uhbd2c90841o7&response_type=token&redirect_uri=https://l-ward.github.io/Project1/"
+    $(location).attr("href", meetupAuthorizationURL);
+} else {
+    var meetupAccessToken = location.href.split("#")[1];
+    var meetupURL = "https://api.meetup.com/2/events?access_token=" + meetupAccessToken + "&group_urlname=books";
+
+    $.ajax({
+        url: meetupURL,
+        method: "GET"
+    }).then(function (res) {
+        console.log(res);
+    });
+}
