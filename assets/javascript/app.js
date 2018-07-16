2590a372a7fb9a6ac40b06c1d07510c3340fd333
Sheckerol <checol70@gmail.com>
2018-07-16 14:42:11 -0700

    made sorting function, ready function and also got ajax working if that wasn't in the last one

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     var events = []; 
+    // this var is for testing to see if we got both our ajax calls back.
+    var readyCheck = 0;
 
     class Event {
         constructor(name, date, link, info) {
@@ -16,6 +18,8 @@ $(document).ready(function () {
     var zipcode = "85302"
     //needs mi or km as suffix
     var distance = 5
+
+
     function getEventBrite() {
         // our search term
 
@@ -29,32 +33,34 @@ $(document).ready(function () {
             res.top_match_events.forEach(element => {
                 formatEventBriteData(element)
             });
+            isReady()
         })
     }
     getEventBrite()
     //new Event(da)
     function formatEventBriteData(event) {
-        e = new Event(event.name, event.start.local, event.url,event.description.text)
-    // this is where we will make all the stuff uniform so that we can compare values easily in sortEvents
+        date = moment(event.start.local,"YYYY-MM-DD HH:mm:ss")
+        e = new Event(event.name, date, event.url,event.description.text)
+        // this is where we will make all the stuff uniform so that we can compare values easily in sortEvents
+    }
+    function isReady(){
+        readyCheck++
+        if(readyCheck === 2){
+            sortEvents()
+        }
     }
     function sortEvents() {
+        readyCheck = 0;
+        events.sort(function(a,b){
+            var adate= a.date
+            var bdate = b.date
+            if(adate.isBefore(bdate)){
+                return -1
+            }else if(adate.isSame(bdate)){
+                return 0
+            }
+            else return 1
+        })
     // this is where we would sort the events by date.
     }
-    // //Checking for acces token
-    // // if (location.href.indexOf("#") == -1) {
-    // //     //OAuth2 seetup for meetup
-    // //     var meetupAuthorizationURL = "https://secure.meetup.com/oauth2/authorize?client_id=idg65cddfd4a7uhbd2c90841o7&response_type=token&redirect_uri=https://l-ward.github.io/Project1/"
-    // //     $(location).attr("href", meetupAuthorizationURL);
-    // // } else {
-    // //     var meetupAccessToken = location.href.split("#")[1];
-    // //     console.log(meetupAccessToken);
-    // //     var meetupURL = "https://api.meetup.com/find/groups?" + meetupAccessToken + "&text=tech";
-
-    // //     $.ajax({
-    // //         url: meetupURL,
-    // //         method: "GET"
-    // //     }).then(function (res) {
-    // //         console.log(res);
-    // //     });
-    // // }
 }); 
