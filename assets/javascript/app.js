a28e7230912f2a482fa3e7d28b0899ea23a6b664
L-Ward <noreply>
2018-07-17 12:23:45 -0700

    working on meetup favorites

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,17 @@
 $(document).ready(function () {
-    var events = []; 
+    var events = [];
     // this var is for testing to see if we got both our ajax calls back.
     var readyCheck = 0;
 
     class Event {
-        constructor(name, date, link, info) {
+        constructor(name, date, link, info, src, id, urlName) {
             this.name = name;
             this.date = date;
             this.link = link;
             this.info = info;
+            this.src = src;
+            this.id = id;
+            this.urlName = urlName;
             events.push(this);
             console.log(this)
         }
@@ -29,7 +32,6 @@ $(document).ready(function () {
             url: eventBriteURL,
             method: "GET"
         }).then(function (res) {
-            // console.log(res);
             res.top_match_events.forEach(element => {
                 formatEventBriteData(element)
             });
@@ -39,29 +41,29 @@ $(document).ready(function () {
     getEventBrite()
     //new Event(da)
     function formatEventBriteData(event) {
-        date = moment(event.start.local,"YYYY-MM-DD HH:mm:ss")
-        e = new Event(event.name, date, event.url,event.description.text)
+        date = moment(event.start.local, "YYYY-MM-DD HH:mm:ss")
+        e = new Event(event.name, date, event.url, event.description.text)
         // this is where we will make all the stuff uniform so that we can compare values easily in sortEvents
     }
-    function isReady(){
+    function isReady() {
         readyCheck++
-        if(readyCheck === 2){
+        if (readyCheck === 2) {
             sortEvents()
         }
     }
     function sortEvents() {
         readyCheck = 0;
-        events.sort(function(a,b){
-            var adate= a.date
+        events.sort(function (a, b) {
+            var adate = a.date
             var bdate = b.date
-            if(adate.isBefore(bdate)){
+            if (adate.isBefore(bdate)) {
                 return -1
-            }else if(adate.isSame(bdate)){
+            } else if (adate.isSame(bdate)) {
                 return 0
             }
             else return 1
         })
-    // this is where we would sort the events by date.
+        // this is where we would sort the events by date.
     }
 
     //MEETUP 
@@ -83,10 +85,22 @@ $(document).ready(function () {
 
     getMeetUp();
 
-    //newEvent
+    //Meetup - newEvent
     function formatMeetUp(event) {
         var date = moment(event.next_event.time).format("MMMM DD YYYY hh:mm a");
-        newEvent = new Event(event.name, date, event.link, event.next_event.name);
+        newEvent = new Event(event.name, date, event.link, event.next_event.name, "meetup", event.next_event.id, event.urlname);
     }
 
+    //Meetup favorites 
+    function returnMeetupFav(id, urlName) {
+        var pre = "https://cors-anywhere.herokuapp.com/";
+        var meetupKey = "221a475e5932e6c6c497a294d424e30";
+        var meetupURL = pre + "api.meetup.com/" + urlName + "/events/" + id + "?key=" + meetupKey + "&photo-host=public";
+        $.ajax({
+            url: meetupURL,
+            method: "GET"
+        }).then(function (res) {
+            formatMeetUp(res);
+        });
+    }
 }); 
